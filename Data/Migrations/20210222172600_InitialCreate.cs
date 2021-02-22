25150adc82ba953778c85d3e7d9e1eb78cfa7e6f
htr97 <carmenreyes@unitec.com>
2021-02-22 14:43:05 -0600

    Ajustes inciales bdd

---
+++ Data/Migrations/20210222172600_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Company = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telephone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Entities/AppUser.cs
@@ -0,0 +1,12 @@
+namespace Entities
+{
+    public class AppUser
+    {
+         public int Id { get; set; }
+        public string UserName{ get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+        public string Company { get; set; }
+        public string Telephone { get; set; }
+    }
+}
